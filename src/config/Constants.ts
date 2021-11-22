4e9ed273b534ff837059be91ae617092ae6a2707
lucaszacharias <lucaszacharias@hotmail.com>
2021-11-22 14:26:14 -0300

    Initial commit

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mmongodb+srv://admin:admin@cluster0.a056i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+}
